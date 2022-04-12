3b36616322d61c3db5eed5a81dfaeb635298427c
briesberg <102183359+briesberg@users.noreply.github.com>
2022-04-11 21:53:22 -0500

    Update Homework 3 assignment(INCOMPLETE CURRENTLY).py

--- Homework 3 assignment(INCOMPLETE CURRENTLY).py
+++ Homework 3 assignment(INCOMPLETE CURRENTLY).py
@@ -1,38 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr  9 18:35:35 2022
-
-@author: benri
-"""
-
-#Part I:
-#Instructor's github: github.com/CinderZhang¶
-#My github:
-#Part II: Working with SEC Filing
-#Following the step in Homework 2,
-#a. Using Python, download 10Ks (5 years if available) of those stocks;
-#b. Using Python, download N-CSR of the funds (1 or 2 funds, exclude index funds) which hold the stocks of interest;
-#c. Parse the 10Ks and extract MD&A and any other section(s) you may need;
-#d. Perform sentiment analysis on the sections;
-#e. Perform sentiment analysis from N-CSR of the stock;
-#f. Analysis the stock performance and write up of your findings.
-#i. What was the sentiment(s) Positive/Negative/Neutral?
-#ii. Stock performance after the disclosures?
-#iii. What did you learn and what you would like to investigate deeper?
-
-
-
-#!pip install sec_api
 from sec_api import QueryApi
 from sec_api import ExtractorApi
 import pandas as pd
 
 
-queryApi = QueryApi(api_key="insert api key here")
+queryApi = QueryApi(api_key="9da9b5776ef77dad3732e5e9d839ef14b409d6c927739c4689520b43993637be")
 
 query = {
   "query": { "query_string": { 
-      "query": "ticker:AAPL AND filedAt:{2016-01-01 TO 2020-12-31} AND formType:\"10-K\"" 
+      "query": "ticker:AAPL AND filedAt:{2020-01-01 TO 2020-12-31} AND formType:\"10-K\"" 
     } }
   ,
   "from": "0",
@@ -42,7 +17,7 @@ query = {
   }
 query_2 ={
   "query": { "query_string": { 
-      "query": "ticker:COKE AND filedAt:{2016-01-01 TO 2020-12-31} AND formType:\"10-K\"" 
+      "query": "ticker:COKE AND filedAt:{2020-01-01 TO 2020-12-31} AND formType:\"10-K\"" 
     } }
   ,
   "from": "0",
@@ -51,7 +26,7 @@ query_2 ={
   }
 query_3 ={
   "query": { "query_string": { 
-      "query": "ticker:GOOGL AND filedAt:{2016-01-01 TO 2020-12-31} AND formType:\"10-K\"" 
+      "query": "ticker:GOOGL AND filedAt:{2020-01-01 TO 2020-12-31} AND formType:\"10-K\"" 
     } }
   ,
   "from": "0",
@@ -60,7 +35,7 @@ query_3 ={
   }
 query_4 ={
   "query": { "query_string": { 
-      "query": "ticker:JPM AND filedAt:{2016-01-01 TO 2020-12-31} AND formType:\"10-K\"" 
+      "query": "ticker:JPM AND filedAt:{2020-01-01 TO 2020-12-31} AND formType:\"10-K\"" 
     } }
   ,
   "from": "0",
@@ -69,13 +44,31 @@ query_4 ={
   }
 query_5 ={
   "query": { "query_string": { 
-      "query": "ticker:NVDA AND filedAt:{2016-01-01 TO 2020-12-31} AND formType:\"10-K\"" 
+      "query": "ticker:NVDA AND filedAt:{2020-01-01 TO 2020-12-31} AND formType:\"10-K\"" 
     } }
   ,
   "from": "0",
   "size": "10",
   "sort": [{ "filedAt": { "order": "desc" } }]
   }
+query_6 ={
+    "query": { "query_string": { 
+        "query": "ticker:VOOG AND filedAt:{2020-01-01 TO 2020-12-31} AND formType:\"N-CSR\"" 
+      } }
+    ,
+    "from": "0",
+    "size": "10",
+    "sort": [{ "filedAt": { "order": "desc" } }]
+    }
+query_7 ={
+    "query": { "query_string": { 
+        "query": "ticker:VOOV AND filedAt:{2020-01-01 TO 2020-12-31} AND formType:\"N-CSR\"" 
+      } }
+    ,
+    "from": "0",
+    "size": "10",
+    "sort": [{ "filedAt": { "order": "desc" } }]
+    }
 
 queries = pd.read_csv("C:/Users/benri/FIN 510/10k- api work/Query list.csv")
 
@@ -86,34 +79,56 @@ filings_coke = queryApi.get_filings(query_2)
 df_coke = pd.json_normalize(filings_coke['filings'])
 filings_googl = queryApi.get_filings(query_3)
 df_googl = pd.json_normalize(filings_googl['filings'])
-df_googl = df_googl.drop([1,5])
-df_googl = df_googl.reset_index()
-df_googl = df_googl.drop(columns=('index'))
+#df_googl = df_googl.drop([1,5])
+#df_googl = df_googl.reset_index()
+#df_googl = df_googl.drop(columns=('index'))
 
 filings_jpm = queryApi.get_filings(query_4)
 df_jpm = pd.json_normalize(filings_jpm['filings'])
 filings_nvda = queryApi.get_filings(query_5)
 df_nvda = pd.json_normalize(filings_nvda['filings'])
 
+#filings_voog = queryApi.get_filings(query_6)
+#df_voog = pd.json_normalize(filings_voog['filings'])
+#filings_voov = queryApi.get_filings(query_7)
+#df_voov = pd.json_normalize(filings_voov['filings'])
 
 filing_url_aapl = df_aapl['linkToFilingDetails'].to_frame()
 filing_url_coke = df_coke['linkToFilingDetails'].to_frame()
 filing_url_googl = df_googl['linkToFilingDetails'].to_frame()
 filing_url_jpm = df_jpm['linkToFilingDetails'].to_frame()
 filing_url_nvda = df_nvda['linkToFilingDetails'].to_frame()
+#filing_url_voog = df_voog['linkToFilingDetails'].to_frame()
+#filing_url_voov = df_voov['linkToFilingDetails'].to_frame()
+
 all_dfs = [filing_url_aapl, filing_url_coke, filing_url_googl, filing_url_jpm, filing_url_nvda]
 
-number_list = list(range(1,11))
 
 df_all = pd.concat(all_dfs).reset_index(drop=True)
 
 #%%
 
+final_10k = []
+
 for url in df_all.index:
-     extractorApi = ExtractorApi("insert api key here")
-     section_10k_item7 = extractorApi.get_section(df_all['linkToFilingDetails'][url],"6",'text')
-     print(section_10k_item7)
-     with open('C:/Users/benri/FIN 510/10k- api work/TSLA_10k_item2.txt', "w") as f:
-         f.write(section_10k_item7)
-     
-    
+     extractorApi = ExtractorApi("9da9b5776ef77dad3732e5e9d839ef14b409d6c927739c4689520b43993637be")
+     section_10k_item7 = extractorApi.get_section(df_all['linkToFilingDetails'][url],"7",'text')
+     final_10k.append(section_10k_item7)
+
+def txt2sentence(txt):
+    import nltk
+    nltk.download('punkt')
+    from nltk.tokenize import sent_tokenize
+    sentences = sent_tokenize(txt)
+    df=pd.DataFrame(sentences)
+    return df  
+
+results=[]
+
+for string in final_10k:
+    for i in string:
+        text = string
+        df_sentence = txt2sentence(text)
+        results.append(df_sentence)
+        print(text)
+  
